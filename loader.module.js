cadf78c5a7100061cefa40de83f04135fd79f1bb
Aaron Santiago <me@aaron.work>
2021-03-12 20:05:13 -0500

    Validate multiple SVG spawning and separate SVG loading/instantiating into its own module

---
+++ loader.module.js
@@ -0,0 +1,119 @@
+
+
+import * as THREE from './libs/three.module.js';
+import {
+  SVGLoader
+}
+from './libs/SVGLoader.js';
+
+let objects = {};
+
+function loadSVG(url) {
+
+  // //
+
+  // scene = new THREE.Scene();
+  // scene.background = new THREE.Color(0xb0b0b0);
+
+  // //
+
+  const loader = new SVGLoader();
+
+  loader.load(url, function (data) {
+    objects[url] = data;
+  });
+
+}
+
+function isLoaded(url) {
+	return url in objects;
+}
+
+function createObject(url, drawFillShapes = true, drawStrokes = true) {
+
+  const paths = objects[url].paths;
+
+  const containerGroup = new THREE.Group();
+  const group = new THREE.Group();
+  group.scale.multiplyScalar(0.25);
+  group.position.x =  - 70;
+  group.position.y = 70;
+  group.scale.y *=  - 1;
+
+  for (let i = 0; i < paths.length; i++) {
+
+    const path = paths[i];
+
+    const fillColor = path.userData.style.fill;
+    if (drawFillShapes && fillColor !== undefined && fillColor !== 'none') {
+
+      const material = new THREE.MeshBasicMaterial({
+          color: new THREE.Color().setStyle(fillColor),
+          opacity: path.userData.style.fillOpacity,
+          transparent: path.userData.style.fillOpacity < 1,
+          side: THREE.DoubleSide,
+          depthWrite: false,
+          wireframe: false
+        });
+
+      const shapes = path.toShapes(true);
+
+      for (let j = 0; j < shapes.length; j++) {
+
+        const shape = shapes[j];
+
+        const geometry = new THREE.ShapeGeometry(shape);
+        const mesh = new THREE.Mesh(geometry, material);
+
+        group.add(mesh);
+
+      }
+
+    }
+
+    const strokeColor = path.userData.style.stroke;
+
+    if (drawStrokes && strokeColor !== undefined && strokeColor !== 'none') {
+
+      const material = new THREE.MeshBasicMaterial({
+          color: new THREE.Color().setStyle(strokeColor),
+          opacity: path.userData.style.strokeOpacity,
+          transparent: path.userData.style.strokeOpacity < 1,
+          side: THREE.DoubleSide,
+          depthWrite: false,
+          wireframe: false
+        });
+
+      for (let j = 0, jl = path.subPaths.length; j < jl; j++) {
+
+        const subPath = path.subPaths[j];
+
+        const geometry = SVGLoader.pointsToStroke(subPath.getPoints(), path.userData.style);
+
+        if (geometry) {
+
+          const mesh = new THREE.Mesh(geometry, material);
+
+          group.add(mesh);
+
+        }
+
+      }
+
+    }
+
+  }
+
+  containerGroup.add(group);
+  // scene.add(containerGroup);
+  return containerGroup;
+}
+
+let index = {
+	loadSVG: loadSVG,
+	createObject: createObject,
+	isLoaded: isLoaded
+};
+
+export { loadSVG, createObject, isLoaded };
+export default index;
--- main.module.js
+++ main.module.js
@@ -9,12 +9,12 @@ import {
 }
 from './libs/dat.gui.module.js';
 // import { OrbitControls } from './libs/OrbitControls.js';
-import {
-  SVGLoader
-}
-from './libs/SVGLoader.js';
+
+import * as Loader from './loader.module.js';
 
 let renderer, stats, scene, camera, gui, guiData;
+let cloudUrl = 'assets/cloud2.svg';
+let loaded = false;
 
 init();
 animate();
@@ -24,6 +24,11 @@ animate();
 function init() {
 
   const container = document.getElementById('container');
+
+  //
+
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xb0b0b0);
 
   //
 
@@ -42,108 +47,16 @@ function init() {
   window.addEventListener('resize', onWindowResize);
 
   guiData = {
-    currentURL: 'assets/cloud2.svg',
     drawFillShapes: true,
     drawStrokes: true,
     fillShapesWireframe: false,
     strokesWireframe: false
   };
 
-  loadSVG(guiData.currentURL);
+  Loader.loadSVG(cloudUrl);
 
 }
 
-function loadSVG(url) {
-
-  //
-
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0xb0b0b0);
-
-  //
-
-  const loader = new SVGLoader();
-
-  loader.load(url, function (data) {
-
-    const paths = data.paths;
-
-    const group = new THREE.Group();
-    group.scale.multiplyScalar(0.25);
-    group.position.x =  - 70;
-    group.position.y = 70;
-    group.scale.y *=  - 1;
-
-    for (let i = 0; i < paths.length; i++) {
-
-      const path = paths[i];
-      console.log(path.userData.node.parentElement.id);
-
-      const fillColor = path.userData.style.fill;
-      if (guiData.drawFillShapes && fillColor !== undefined && fillColor !== 'none') {
-
-        const material = new THREE.MeshBasicMaterial({
-            color: new THREE.Color().setStyle(fillColor),
-            opacity: path.userData.style.fillOpacity,
-            transparent: path.userData.style.fillOpacity < 1,
-            side: THREE.DoubleSide,
-            depthWrite: false,
-            wireframe: guiData.fillShapesWireframe
-          });
-
-        const shapes = path.toShapes(true);
-
-        for (let j = 0; j < shapes.length; j++) {
-
-          const shape = shapes[j];
-
-          const geometry = new THREE.ShapeGeometry(shape);
-          const mesh = new THREE.Mesh(geometry, material);
-
-          group.add(mesh);
-
-        }
-
-      }
-
-      const strokeColor = path.userData.style.stroke;
-
-      if (guiData.drawStrokes && strokeColor !== undefined && strokeColor !== 'none') {
-
-        const material = new THREE.MeshBasicMaterial({
-            color: new THREE.Color().setStyle(strokeColor),
-            opacity: path.userData.style.strokeOpacity,
-            transparent: path.userData.style.strokeOpacity < 1,
-            side: THREE.DoubleSide,
-            depthWrite: false,
-            wireframe: guiData.strokesWireframe
-          });
-
-        for (let j = 0, jl = path.subPaths.length; j < jl; j++) {
-
-          const subPath = path.subPaths[j];
-
-          const geometry = SVGLoader.pointsToStroke(subPath.getPoints(), path.userData.style);
-
-          if (geometry) {
-
-            const mesh = new THREE.Mesh(geometry, material);
-
-            group.add(mesh);
-
-          }
-
-        }
-
-      }
-
-    }
-
-    scene.add(group);
-
-  });
-
-}
 
 function onWindowResize() {
 
@@ -157,6 +70,17 @@ function onWindowResize() {
 function animate() {
 
   requestAnimationFrame(animate);
+
+  if (Loader.isLoaded(cloudUrl) && !loaded) {
+    loaded = true;
+    for (let x = -100; x < 30; x += 1.3) {
+      let cloud = Loader.createObject(cloudUrl);
+      cloud.position.x = x;
+      cloud.position.y = Math.sin(x/10) * 10 + x/20;
+      cloud.scale.multiplyScalar((x + 200)/ 230);
+      scene.add(cloud);
+    }
+  }
 
   render();
 
