06a54557b90dc1d8d563e8a549ab5bb35ed044fe
Aaron Santiago <me@aaron.work>
2021-03-16 14:37:47 -0400

    First pass at rule parsing

--- main.module.js
+++ main.module.js
@@ -15,6 +15,7 @@ import * as Loader from './loader.module.js';
 let renderer, stats, scene, camera, gui, guiData;
 let cloudUrl = 'assets/cloud2.svg';
 let loaded = false;
+let rules = {};
 
 init();
 animate();
@@ -32,7 +33,14 @@ function init() {
 
   //
 
-  camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+  // camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 1000);
+  camera = new THREE.OrthographicCamera(
+            -50 * window.innerWidth / window.innerHeight,
+            50 * window.innerWidth / window.innerHeight,
+            50,
+            -50,
+            .1,
+            5000);
   camera.position.set(0, 0, 200);
 
   //
@@ -54,13 +62,89 @@ function init() {
   };
 
   Loader.loadSVG(cloudUrl);
+}
 
+function processRule(rule, currentDimensions) {
+  console.log("processing rule:");
+  console.log(rule);
+  console.log(currentDimensions);
+  if ("spawn" in rule) {
+    for (let spawn of rule["spawn"]) {
+      for (let i = 0; i < spawn["count"]; i++) {
+        let spawnedObj = Loader.createObject("assets/" + spawn["url"]);
+        let size = 1;
+        if ("size" in spawn) size = spawn["size"];
+        if ("sizeType" in spawn) {
+          if (spawn["sizeType"] == "absolute") {
+            spawnedObj.scale.multiplyScalar(size);
+          }
+          else if (spawn["sizeType"] == "relative") {
+            let relScale = Math.min((currentDimensions.right - currentDimensions.left), (currentDimensions.top - currentDimensions.bottom));
+            spawnedObj.scale.x = relScale * size;
+            spawnedObj.scale.y = relScale * size;
+          }
+        }
+        spawnedObj.position.x = (currentDimensions.left + (currentDimensions.right - currentDimensions.left))/2 * 100 - 50;
+        spawnedObj.position.y = (currentDimensions.bottom + (currentDimensions.top - currentDimensions.bottom))/2 * 100 - 50;
+        scene.add(spawnedObj);
+        console.log("spawning!");
+        console.log(spawnedObj.position);
+        console.log(spawnedObj.scale);
+      }
+    }
+  }
+  if ("replace" in rule) {
+    let lastDimensions = {
+      "left": 0,
+      "right": 1,
+      "bottom": 0,
+      "top": 1
+    };
+    for (let replace of rule["replace"]) {
+      let newDimensions = Object.assign({}, currentDimensions);
+      function tryReplaceThenReturnIfNumerical(dir) {
+        if (dir in replace) {
+          if ((typeof replace[dir] === 'string' || replace[dir] instanceof String)) {
+            if (replace[dir].startsWith("previous_")) {
+              newDimensions[dir] = lastDimensions[replace[dir].split("_")[1]];
+            }
+            return false;
+          } 
+          return true;
+        }
+        return false;
+      }
+      if (tryReplaceThenReturnIfNumerical("left")) {
+        newDimensions.left = 
+          (currentDimensions.right - currentDimensions.left) * replace.left + currentDimensions.left;
+      }
+      if (tryReplaceThenReturnIfNumerical("right")) {
+        newDimensions.right = 
+          (currentDimensions.right - currentDimensions.left) * replace.right + currentDimensions.left;
+      }
+      if (tryReplaceThenReturnIfNumerical("bottom")) {
+        newDimensions.bottom = 
+          (currentDimensions.top - currentDimensions.bottom) * replace.bottom + currentDimensions.bottom;
+      }
+      if (tryReplaceThenReturnIfNumerical("top")) {
+        newDimensions.top = 
+          (currentDimensions.top - currentDimensions.bottom) * replace.top + currentDimensions.bottom;
+          // (currentDimensions.bottom - currentDimensions.top) * replace.top + currentDimensions.top;
+      }
+      processRule(rules[replace.id], newDimensions);
+      lastDimensions = newDimensions;
+    }
+  }
 }
 
 
 function onWindowResize() {
 
-  camera.aspect = window.innerWidth / window.innerHeight;
+  // camera.aspect = window.innerWidth / window.innerHeight;
+  camera.left = -50 * window.innerWidth / window.innerHeight;
+  camera.right = 50 * window.innerWidth / window.innerHeight;
+  camera.top = 50;
+  camera.bottom = -50;
   camera.updateProjectionMatrix();
 
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -73,17 +157,37 @@ function animate() {
 
   if (Loader.isLoaded(cloudUrl) && !loaded) {
     loaded = true;
-    for (let x = -100; x < 30; x += 1.3) {
-      let cloud = Loader.createObject(cloudUrl);
-      cloud.position.x = x;
-      cloud.position.y = Math.sin(x/10) * 10 + x/20;
-      cloud.scale.multiplyScalar((x + 200)/ 230);
-      scene.add(cloud);
+    // for (let x = -100; x < 30; x += 1.3) {
+    //   let cloud = Loader.createObject(cloudUrl);
+    //   cloud.position.x = x;
+    //   cloud.position.y = Math.sin(x/10) * 10 + x/20;
+    //   cloud.scale.multiplyScalar((x + 200)/ 230);
+    //   scene.add(cloud);
+    // }
+
+    let rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", "rules/sky.json", true);
+    rawFile.onreadystatechange = function () {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+        // initialize
+        let parsedRules = JSON.parse(rawFile.responseText);
+        rules = parsedRules["rules"];
+        let current = {
+          "left": 0,
+          "right": 1,
+          "bottom": 0,
+          "top": 1
+        };
+        processRule(rules[parsedRules["initial"][0]["id"]], current);
+
+
+      }
     }
+    rawFile.send(null);
   }
 
   render();
-
 }
 
 function render() {
